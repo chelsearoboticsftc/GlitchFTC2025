49f81f6777ce8f9473959920e07a137598d8aa64
Hamgo272 <ke8uee@gmail.com>
2025-09-16 17:41:50 -0400

    index theory code
    added theoretical index code starting basis.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/example/indextest.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.subsystems.example;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+//theoretical index test code
+
+
+public class indextest {
+    ColorSensor pos1, pos2, pos3;
+    DcMotorEx indexmotor;
+
+    public indextest(HardwareMap hardwareMap){
+
+        this.pos1 = hardwareMap.get(ColorSensor.class, "pos1sensor");
+        this.pos2 = hardwareMap.get(ColorSensor.class, "pos2sensor");
+        this.pos3 = hardwareMap.get(ColorSensor.class, "pos3sensor");
+        this.indexmotor = hardwareMap.get(DcMotorEx.class, "indexmotor");
+        indexmotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
+    public void read(){
+
+        if (pos1.argb() == 300){
+            //replace 300 with rgb values for green color ball
+            String pos1color = "green";
+
+        }
+    }
+
+    public void sort(String color){
+        indexmotor.setTargetPosition(100);
+        //set pos for each correct ball color...
+    }
+
+
+}
