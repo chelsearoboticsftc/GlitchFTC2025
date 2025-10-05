e9dd4fbde8343b565089ca0adc0f6d8468bd4b3c
Hamgo272 <ke8uee@gmail.com>
2025-10-05 18:50:44 -0400

    create obelisk april tag detector
    Todo transfer all the code to a subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/visiontest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/visiontest.java
@@ -1,11 +1,14 @@
 package org.firstinspires.ftc.teamcode.opmodes.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.sun.tools.javac.util.List;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.ArrayList;
 //import org.openftc.apriltag.AprilTagDetection;
 
 @TeleOp
@@ -16,25 +19,35 @@ public class visiontest extends LinearOpMode{
         AprilTagProcessor myAprilTagProcessor;
         myAprilTagProcessor = AprilTagProcessor.easyCreateWithDefaults();
         VisionPortal myVisionPortal;
+        ArrayList<AprilTagDetection> detections = new ArrayList<>();
         myVisionPortal = VisionPortal.easyCreateWithDefaults(hardwareMap.get(WebcamName.class, "Webcam 1"), myAprilTagProcessor);
-        int myAprilTagIdCode;
+        String ball_order;
         waitForStart();
         while(opModeIsActive()){
 
 
             if (myAprilTagProcessor.getDetections().size() > 0.0){
-                AprilTagDetection myAprilTagDetection = myAprilTagProcessor.getDetections().get(0);
-                myAprilTagIdCode = myAprilTagDetection.id;
-                
+                myAprilTagProcessor.getDetections();
+                detections = myAprilTagProcessor.getDetections();
+
+
             }
             else {
-                myAprilTagIdCode = 0;
+                detections.clear();
             }
-            telemetry.addData("id", myAprilTagIdCode);
+            telemetry.addData("id", detections);
             telemetry.update();
 
 
-
+            if (detections.contains(21)){
+                ball_order = "gpp";
+            }
+            if (detections.contains(22)){
+                ball_order = "pgp";
+            }
+            if (detections.contains(23)){
+                ball_order = "ppg";
+            }
         }
     }
 
